c14901ea3a1dc89a7efb67ba10aefd53ac8de2b5
Mel <mel@192-168-1-11.tpgi.com.au>
2017-06-04 20:00:16 +1000

    initial push

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Component } from 'react';
+import '../style/App.css';
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
---
+++ src/containers/search_bar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div>
+        <form className="input-group">
+          <input />
+        </form>
+      </div>
+    )
+  }
+}
